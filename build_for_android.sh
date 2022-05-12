0b08d7ddc05f10292ebeacf668b6a9076fa48553
Sujan Shakya <sujan@teslatech.com.np>
2022-05-12 16:48:14 +0545

    Added build scripts for android and osx

---
+++ build_for_android.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd godot-cpp
+
+echo "Generating bindings for android armv7"
+scons platform=android bits=64 android_arch=armv7 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
+
+echo "Generating bindings for android arm64v8"
+scons platform=android bits=64 android_arch=arm64v8 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
+
+cd ..
+
+echo "Generating libgdsqlite for android all arch"
+$ANDROID_NDK_ROOT/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=Android.mk APP_PLATFORM=android-21 NDK_LIBS_OUT=demo/addons/godot-sqlite/bin/android/
---
+++ build_for_osx.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd godot-cpp
+
+echo "Generating bindings for macos arm64"
+scons platform=osx bits=64 macos_arch=arm64 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
+
+echo "Generating bindings for macos x86_64"
+scons platform=osx bits=64 macos_arch=x86_64 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
+
+cd ..
+
+echo "Generating libgdsqlite for macos arm64"
+mkdir -p demo/addons/godot-sqlite/bin/osx_arm64
+scons platform=osx bits=64 macos_arch=arm64 target=release target_path=demo/addons/godot-sqlite/bin/osx_arm64/ target_name=libgdsqlite
+
+echo "Generating libgdsqlite for macos x86_64"
+mkdir -p demo/addons/godot-sqlite/bin/osx_x86_64
+scons platform=osx bits=64 macos_arch=x86_64 target=release target_path=demo/addons/godot-sqlite/bin/osx_x86_64/ target_name=libgdsqlite
