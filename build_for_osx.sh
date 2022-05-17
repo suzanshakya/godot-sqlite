b3e44ecd763b54486569e6be68aa857e04844345
Sujan Shakya <sujan@teslatech.com.np>
2022-05-17 18:29:32 +0545

    Adding universal build for osx

--- build_for_osx.sh
+++ build_for_osx.sh
@@ -10,10 +10,6 @@ scons platform=osx bits=64 macos_arch=x86_64 target=release generate_bindings=ye
 
 cd ..
 
-echo "Generating libgdsqlite for macos arm64"
-mkdir -p demo/addons/godot-sqlite/bin/osx_arm64
-scons platform=osx bits=64 macos_arch=arm64 target=release target_path=demo/addons/godot-sqlite/bin/osx_arm64/ target_name=libgdsqlite
-
-echo "Generating libgdsqlite for macos x86_64"
-mkdir -p demo/addons/godot-sqlite/bin/osx_x86_64
-scons platform=osx bits=64 macos_arch=x86_64 target=release target_path=demo/addons/godot-sqlite/bin/osx_x86_64/ target_name=libgdsqlite
+echo "Generating libgdsqlite for macos universal"
+mkdir -p demo/addons/godot-sqlite/bin/osx
+scons platform=osx bits=64 macos_arch=universal target=release target_path=demo/addons/godot-sqlite/bin/osx/ target_name=libgdsqlite
