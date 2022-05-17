8b09d5fad5a3246579adf1f8fa74ad88fe727289
nabinbogati <nabin@teslatech.com.np>
2022-05-17 12:17:11 +0000

    Merged conflicts

--- build_for_android.sh
+++ build_for_android.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
 export ANDROID_NDK_ROOT=~/Library/Android/android-ndk-r22b
+#export ANDROID_NDK_ROOT=/home/callbreak/android/android-ndk-r22b
 
 cd godot-cpp
 
 echo "Generating bindings for android armv7"
 scons platform=android bits=64 android_arch=armv7 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
+#scons platform=android bits=64 android_arch=armv7 target=release generate_bindings=yes -j 1
 
 echo "Generating bindings for android arm64v8"
 scons platform=android bits=64 android_arch=arm64v8 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
+#scons platform=android bits=64 android_arch=arm64v8 target=release generate_bindings=yes -j 1
 
 #echo "Generating bindings for android x86"
 #scons platform=android bits=64 android_arch=x86 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
-#
+#scons platform=android bits=64 android_arch=x86 target=release generate_bindings=yes -j 1
+
 #echo "Generating bindings for android x86_64"
 #scons platform=android bits=64 android_arch=x86_64 target=release generate_bindings=yes -j $(sysctl -n hw.logicalcpu)
+#scons platform=android bits=64 android_arch=x86_64 target=release generate_bindings=yes -j 1
 
 cd ..
 
