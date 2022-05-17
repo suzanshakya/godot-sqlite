6274b6dd5100bff020940581524ff55b8f3ff402
Sujan Shakya <sujan@teslatech.com.np>
2022-05-17 22:45:04 +0545

    Scripts finalized for osx

--- install.sh
+++ install.sh
@@ -3,8 +3,13 @@
 CBROOT=~/projects/teslatech/callbreakgodot
 
 
-echo "cp demo/addons/godot-sqlite/bin/osx/libgdsqlite.dylib $CBROOT/addons/godot-sqlite/bin/osx/"
-cp demo/addons/godot-sqlite/bin/osx/libgdsqlite.dylib $CBROOT/addons/godot-sqlite/bin/osx/
+mkdir -p $CBROOT/addons/godot-sqlite/bin/osx_arm64/
+echo "cp demo/addons/godot-sqlite/bin/osx_arm64/libgdsqlite.dylib $CBROOT/addons/godot-sqlite/bin/osx_arm64/"
+cp demo/addons/godot-sqlite/bin/osx_arm64/libgdsqlite.dylib $CBROOT/addons/godot-sqlite/bin/osx_arm64/
+
+mkdir -p $CBROOT/addons/godot-sqlite/bin/osx_x86_64/
+echo "cp demo/addons/godot-sqlite/bin/osx_x86_64/libgdsqlite.dylib $CBROOT/addons/godot-sqlite/bin/osx_x86_64/"
+cp demo/addons/godot-sqlite/bin/osx_x86_64/libgdsqlite.dylib $CBROOT/addons/godot-sqlite/bin/osx_x86_64/
 
 echo "cp demo/addons/godot-sqlite/bin/android/armeabi-v7a/libgdsqlite.so $CBROOT/addons/godot-sqlite/bin/android/armeabi-v7a/"
 cp demo/addons/godot-sqlite/bin/android/armeabi-v7a/libgdsqlite.so $CBROOT/addons/godot-sqlite/bin/android/armeabi-v7a/
